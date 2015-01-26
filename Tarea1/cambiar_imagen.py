1cf21bcb69f6100a5bc3da0d292c537f5af6149c
Kevin Michelle Contreras González <michelleglz93@gmail.com>
2015-01-25 22:11:42 -0600

    3

--- Tarea1/cambiar_imagen.py
+++ Tarea1/cambiar_imagen.py
@@ -3,14 +3,13 @@ import ImageDraw
 import sys
 import os.path
 import operator
-from time import sleep
 
 def encontrar_colores(imagen):
 	colores = {}
 	w, h = imagen.size
 	pixeles = imagen.load()
-	for x in xrange(w):
-		for y in xrange(h):
+	for y in xrange(h):
+		for x in xrange(w):
 			valor_pixel = pixeles[x, y]
 			if valor_pixel not in colores:
 				colores[valor_pixel] = 1
@@ -21,9 +20,9 @@ def encontrar_colores(imagen):
 def prueba_colores(imagen):
 	w, h = imagen.size
 	pixeles = imagen.load()
-	for x in xrange(w):
-		for y in xrange(h):
-			if (x == 0 and y == 0) or (x == 0 and y == h - 1) or (x == w - 1 and y == 0) or (x == w - 1 and y == h - 1):
+	for y in xrange(h):
+		for x in xrange(w):
+			if (y == 0 and x == 0) or (y == 0 and x == w - 1) or (y == h - 1 and x == 0) or (y == h - 1 and x == w - 1):
 				if (x == 0 and y == 0):
 					pixeles[x, y]  = (255, 0, 0)
 				elif (x == 0 and y == h - 1):
@@ -38,8 +37,9 @@ def prueba_colores(imagen):
 def aplicar_filtro(imagen):
 	w, h = imagen.size
 	pixeles = imagen.load()
-	for x in xrange(w):
-		for y in xrange(h):
+	for y in xrange(h):
+		for x in xrange(w):
+			print x, y, pixeles[x, y]
 			encontrar_vecinos(imagen, x, y)
 
 def encontrar_vecinos(imagen, x_pixel, y_pixel):
@@ -48,33 +48,30 @@ def encontrar_vecinos(imagen, x_pixel, y_pixel):
 	azul_vecinos = []
 	pixeles = imagen.load()
 	w, h = imagen.size
-	print "pixel: ", x_pixel, y_pixel
-	for x in xrange(x_pixel - 1, x_pixel + 2):
-		if x >= 0 and x < w:
-			for y in xrange(y_pixel - 1, y_pixel + 2):
-				if y >= 0 and y < h:
-					print "hola", x, y
+	for y in xrange(y_pixel - 1, y_pixel + 2):
+		if y >= 0 and y < h:
+			for x in xrange(x_pixel - 1, x_pixel + 2):
+				print "x", x
+				if x >= 0 and x < w:
 					pixel = pixeles[x, y]
 					rojo_vecinos.append(pixel[0])
 					verde_vecinos.append(pixel[1])
 					azul_vecinos.append(pixel[2])
+	print
 	print "rojos", rojo_vecinos
 	print "verde", verde_vecinos
 	print "azul", azul_vecinos
+	print
 
 if len(sys.argv) == 2:
 	filename = sys.argv[1]
 	if os.path.isfile(filename):
-		#imagen = Image.open(filename)
-		imagen = Image.new('RGB', (5, 5))
+		imagen = Image.open(filename)
+		#imagen = Image.new('RGB', (5, 5))
 		draw = ImageDraw.Draw(imagen)
 		imagen_datos = imagen.getdata()
-		hola1 = encontrar_colores(imagen)
-		print hola1
-		hola2 = encontrar_colores(imagen)
-		print hola2
-		prueba_colores(imagen)
 		aplicar_filtro(imagen)
+		prueba_colores(imagen)
 	else:
 		print "No existe el archivo %s" %filename
 else:
